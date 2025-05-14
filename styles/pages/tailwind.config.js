336e0aa0fd675e3b8446d6a4b6b1bf94ca064ec3
Revisr-bit <Jamaalderby@gmail.com>
2025-05-14 18:25:21 +0100

    Update tailwind.config.js

--- styles/pages/tailwind.config.js
+++ styles/pages/tailwind.config.js
@@ -1,14 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#A274FF"
-      }
-    }
+    extend: {},
   },
   plugins: [],
 };
+
